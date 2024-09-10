2f5277b99e6c03eb74b40461bc3fa56f7f38e8e9
Kyle Scudder <kyle@kylescudder.co.uk>
2024-09-10 14:08:06 +0000

    fix(install): added -noconfirm to 1Password makepkg

--- install.sh
+++ install.sh
@@ -53,7 +53,7 @@ cd ~/
 curl -sS https://downloads.1password.com/linux/keys/1password.asc | gpg --import
 git clone https://aur.archlinux.org/1password.git
 cd 1password
-makepkg -si
+makepkg -si --noconfirm
 echo "1Password install completed"
 
 # Install snap and snap store
